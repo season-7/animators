abbca4b9d475c31f599829e9de084ee1b39c3cf2
willardshikami <willardshikami@gmail.com>
2016-07-12 07:29:23 +0300

    updated header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+$(".slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('.slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('.slideshow');
+},  6000);
